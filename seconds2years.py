df6637a56003f287e5c6e7022ae495f4537cdca5
Krystal Lee <lee301@mail.chapman.edu>
2016-02-18 14:11:24 -0800

    Exercise 1.3

---
+++ seconds2years.py
@@ -0,0 +1,12 @@
+#Exercise 1.3
+#can a newborn baby in Norway expect to live for one billion (10^9) seconds?
+#write a Python program for doing arithmetics to answer the question.
+
+seconds = 1000000000
+minutes = seconds/60
+hours = minutes/60
+days = hours/24
+years = days/365
+print years
+
+print "the child will live to be %d" % years
