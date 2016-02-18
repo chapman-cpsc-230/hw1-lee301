f14ad6a0e8640b68d9c4e725ac72b6f2375d0766
Krystal Lee <lee301@mail.chapman.edu>
2016-02-18 14:53:23 -0800

    exercise 1.6 & edits

--- seconds2years.py
+++ seconds2years.py
@@ -1,9 +1,9 @@
-File: <seconds2years.py>
+"""File: <seconds2years.py>
 
 Copyright (c) 2016 <Krystal Lee>
 
 License: (http://img.shields.io/badge/license-MIT-blue.svg)](http://en.wikipedia.org/wiki/MIT_License)
-
+"""
 #Exercise 1.3
 #can a newborn baby in Norway expect to live for one billion (10^9) seconds?
 #write a Python program for doing arithmetics to answer the question.
@@ -15,4 +15,4 @@ days = hours/24
 years = days/365
 print years
 
-print "the child will live to be %d" % years
+print "The child will live to be %d." % years
