69de1d61055fd5fd1ab7e5ecb3f7b88343617ee1
Krystal Lee <lee301@mail.chapman.edu>
2016-02-18 14:28:44 -0800

    Edits

--- seconds2years.py
+++ seconds2years.py
@@ -1,3 +1,9 @@
+File: <seconds2years.py>
+
+Copyright (c) 2016 <Krystal Lee>
+
+License: (http://img.shields.io/badge/license-MIT-blue.svg)](http://en.wikipedia.org/wiki/MIT_License)
+
 #Exercise 1.3
 #can a newborn baby in Norway expect to live for one billion (10^9) seconds?
 #write a Python program for doing arithmetics to answer the question.
